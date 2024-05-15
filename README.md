0e5ae4a2f791c9bf3b13d48bc3ad82d234a0f507
Jason Friedman <jsf80238@gmail.com>
2024-05-15 12:01:35 -0600

    .

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # rht_data_engineer
 
-$ grep -h "<status>" *.xml | sort | uniq
-    <status>Completed</status>
-    <status>In Progress</status>
-    <status>Received</status>
-    <status>Reopened</status>
+    $ grep -h "<status>" *.xml | sort | uniq
+        <status>Completed</status>
+        <status>In Progress</status>
+        <status>Received</status>
+        <status>Reopened</status>
+    
+    $ grep -h "<order_id>" *.xml | sort | uniq
+        <order_id>101</order_id>
+        <order_id>102</order_id>
+        <order_id>103</order_id>
+        <order_id>104</order_id>
 
-$ grep -h "<order_id>" *.xml | sort | uniq
-    <order_id>101</order_id>
-    <order_id>102</order_id>
-    <order_id>103</order_id>
-    <order_id>104</order_id>
+Not checking if the records are complete, for example, no order id or order id not numeric
--- rht_data_engineer/run_pipeline.py
+++ rht_data_engineer/run_pipeline.py
@@ -1,34 +1,60 @@
-from io import StringIO
+import logging
+from collections import defaultdict
+from dataclasses import dataclass
+from datetime import datetime
+from enum import StrEnum
 from pathlib import Path
 # Imports above are standard Python
 # Imports below are 3rd-party
-import pandas as pd
 from xml_to_dict import XMLtoDict
-import xml.etree.ElementTree as ET
 # Imports below are custom
 from lib.base import Logger, Database, DATAFILE_LOCATION
 
 
-def iter_docs(author):
-    author_attr = author.attrib
-    for doc in author.iter('document'):
-        doc_dict = author_attr.copy()
-        doc_dict.update(doc.attrib)
-        doc_dict['data'] = doc.text
-        yield doc_dict
+class Tag(StrEnum):
+    EVENT = "event"
+    ORDER_ID = "order_id"
+    DATE_TIME = "date_time"
+    STATUS = "status"
+    COST = "cost"
+    REPAIR_DETAILS = "repair_details"
+    TECHNICIAN = "technician"
+    REPAIR_PARTS = "repair_parts"
+    PART = "part"
+    NAME = "@name"
+    QUANTITY = "@quantity"
 
-# xml_data = open("/home/jason/PycharmProjects/rht_data_engineer/data/shard_12.xml").read()
-# print(xml_data)
-# etree = ET.parse("/home/jason/PycharmProjects/rht_data_engineer/data/shard_12.xml") #create an ElementTree object
-# print(pd.DataFrame(list(iter_docs(etree.getroot()))))
-#
-# exit()
 
-# logger = Logger().get_logger()
+class Status(StrEnum):
+    COMPLETED = "Completed"
+    IN_PROGRESS = "In Progress"
+    RECEIVED = "Received"
+    REOPENED = "Reopened"
+
+
+@dataclass
+class RepairParts:
+    order_id: int
+    part_name: str
+    quantity: int
+
+
+@dataclass
+class RepairOrder:
+    order_id: int
+    timestamp: datetime
+    status: Status
+    cost: float
+    technician: str
+    repair_parts_id: int  # key to repair_parts table 
+
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
+logger = Logger().get_logger()
 # db_connection = Database().get_connection()
 
 
-def read_files_from_dir(folder: [str, Path]) -> list[pd.DataFrame]:
+def read_files_from_dir(folder: [str, Path]) -> list[str]:
     """
 
     :param dir: /full/path/to/dir/containing/data/files
@@ -37,13 +63,41 @@ def read_files_from_dir(folder: [str, Path]) -> list[pd.DataFrame]:
     if isinstance(folder, str):
         folder = Path(str)
     file_list = sorted(folder.glob("*.xml"))
+    logger.info(f"Reading from folder '{folder}' ...")
     for i, file_path in enumerate(file_list, 1):
-        content = open(file_path).read()
-        print(content)
-        parser = XMLtoDict()
-        print(parser.parse(content))
-        exit()
+        logger.info(f"Reading '{file_path}' ({i} of {len(file_list)} ...")
+        yield open(file_path).read()
 
 
-for item in read_files_from_dir(DATAFILE_LOCATION):
-    pass
+def parse_xml(content: str) -> dict:
+    """
+
+    :param content:
+    :return:
+    """
+    parser = XMLtoDict()
+    return parser.parse(content)
+
+
+repair_order_dict = dict(RepairOrder)  # keys are order ID
+repair_parts_dict = dict(RepairParts)  # keys are order ID
+
+for file_content in read_files_from_dir(DATAFILE_LOCATION):
+    try:
+        order_raw = parse_xml(file_content)[Tag.EVENT.value]
+    except Exception as e:
+        logger.error(f"Skipping because of: {e}")
+    order_id = order_raw[Tag.ORDER_ID.value]
+    try:
+        timestamp = order_raw[Tag.DATE_TIME.value]
+        timestamp_as_datetime = datetime.strptime(timestamp, TIMESTAMP_FORMAT)
+        print(timestamp_as_datetime)
+    except Exception as e:
+        logger.error(f"Skipping because could not parse timestamp: {e}")
+    try:
+        existing_timestamp = repair_order_dict[order_id].timestamp
+    except:
+        existing_timestamp = datetime(1, 1, 1)  # Earliest possible date
+        logger.info(f"We are seeing order {order_id} for the first time.")
+    if timestamp_as_datetime > existing_timestamp:
+        pass
